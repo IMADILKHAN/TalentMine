0d3705c6ada3ad18832a769fae8ac95a76c6e8e1
adil khan <blueaadil@gmail.com>
2021-10-18 18:51:00 +0530

    added projects,tag,reviews model

--- devDirectory/projects/urls.py
+++ devDirectory/projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects,name="all projects"),
-    path('project/<str:pk>/', views.project,name="adils projects"),
+    path('', views.projects,name="projects"),
+    path('project/<str:pk>/', views.project,name="project"),
 ]
--- devDirectory/projects/views.py
+++ devDirectory/projects/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+projectsList = [
+        {
+        'id':'1',
+        'title':'Ecommerce Website',
+        'description':'Fully functional ecommerce website'},
+        {
+        'id':'2',
+        'title':'Fayur',
+        'description':"Website To make artist and venur's meet"},
+        {
+        'id':'3',
+        'title':'Craigs List',
+        'description':"Craigs List Clone"},]
+
+
+
+
+
 def projects(request):
-    return render(request,'projects/projects.html')
+    page = 'projects'
+    number = 11
+    context = {'msg':page,'num':number,'projects':projectsList}
+    return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id']==pk:
+            projectObj = i
+            break
+    # print(projectObj)
+    return render(request,'projects/single-project.html',{'project':projectObj})
