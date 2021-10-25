e8acee547996617ded088b3e7209eb0c6e32c618
adil khan <blueaadil@gmail.com>
2021-10-25 19:24:00 +0530

    Added Skills model and linked it to the Profile Model

--- devDirectory/users/models.py
+++ devDirectory/users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     name = models.CharField(max_length = 100,blank=True,null=True)
     email = models.EmailField(max_length=100,blank=True,null=True)
     username  = models.CharField(max_length = 100,blank=True,null=True)
+    location = models.CharField(max_length = 500,blank=True,null=True)
     short_intro  = models.CharField(max_length = 180,blank=True,null=True)
     bio = models.TextField(blank=True,null=True)
     profile_image = models.ImageField(null=True,blank=True,upload_to = 'profiles/',default ="profiles/avatar.jpeg" )
@@ -20,3 +21,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile,on_delete = models.CASCADE,null=True)
+    name = models.CharField(max_length = 100,blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default = uuid.uuid4, unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.name)
