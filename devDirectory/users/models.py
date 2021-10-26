fc4fdafda0b34a47660c178531de7417d4190353
adil khan <blueaadil@gmail.com>
2021-10-26 16:44:36 +0530

    Added user Authentication

--- devDirectory/users/models.py
+++ devDirectory/users/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length = 100,blank=True,null=True)
--- devDirectory/users/views.py
+++ devDirectory/users/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
 
+def loginPage(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('User not registered')
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            print('logingin')
+            login(request,user)
+            return redirect('profiles')
+        else:
+            print('Incorrect Password')
+    return render(request,'users/login_register.html')
 
 def profiles(request):
     profiles = Profile.objects.all()
